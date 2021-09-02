41ab34e6ae34d0c7382e2c4439e1a21aa38ec607
FIC <FIC@info.local>
2021-09-02 17:34:00 -0300

    banco alterado para api

--- 02_08_2021/1.2-exercicio-locadora/scripts/locadora_03_DQL.sql
+++ 02_08_2021/1.2-exercicio-locadora/scripts/locadora_03_DQL.sql
@@ -8,15 +8,36 @@ SELECT * FROM MODELO;
 SELECT * FROM VEICULO;
 SELECT * FROM ALUGUEL;
 
+SELECT nomeEmpresa, nomeModelo, placa FROM VEICULO LEFT JOIN EMPRESA ON EMPRESA.idEmpresa = VEICULO.idEmpresa LEFT JOIN MODELO ON MODELO.idModelo = VEICULO.idEmpresa WHERE idVeiculo = 2;
+
+SELECT A.idVeiculo, dataRetirada, dataDevolucao, CLIENTE.nomeCliente, M.idModelo, M.nomeModelo 
+FROM ALUGUEL A
+LEFT JOIN CLIENTE 
+ON A.idcliente = CLIENTE.idCliente 
+LEFT JOIN VEICULO 
+ON A.idVeiculo = VEICULO.idVeiculo 
+LEFT JOIN MODELO M 
+ON VEICULO.idModelo = M.idModelo
+WHERE idAluguel = 2;
+
+SELECT dataRetirada, dataDevolucao, CLIENTE.nomeCliente, MODELO.nomeModelo FROM ALUGUEL WHERE idAluguel = idAluguel
+FROM ALUGUEL A
+LEFT JOIN CLIENTE 
+ON A.idcliente = CLIENTE.idCliente 
+LEFT JOIN VEICULO 
+ON A.idVeiculo = VEICULO.idVeiculo 
+LEFT JOIN MODELO M 
+ON VEICULO.idModelo = M.idModelo
 
 
-SELECT dataRetirada, dataDevolucao, CLIENTE.nomeCliente, MODELO.nomeModelo FROM ALUGUEL
+SELECT idAluguel, dataRetirada, dataDevolucao, CLIENTE.nomeCliente, MODELO.nomeModelo FROM ALUGUEL 
 LEFT JOIN CLIENTE
 ON ALUGUEL.idcliente = CLIENTE.idCliente
 LEFT JOIN VEICULO
 ON ALUGUEL.idVeiculo = VEICULO.idVeiculo
 LEFT JOIN MODELO
-ON VEICULO.idModelo = MODELO.idModelo;
+ON VEICULO.idModelo = MODELO.idModelo
+WHERE idAluguel = 2;
 
 
                     
