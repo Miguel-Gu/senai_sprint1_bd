668bbef2a875569bad4f7e87e4aa400ad88d3ef0
Gustavo <gumiguel8@hotmail.com>
2021-09-03 18:55:11 -0300

    banco da api pronta

--- 02_08_2021/1.2-exercicio-locadora/scripts/locadora_03_DQL.sql
+++ 02_08_2021/1.2-exercicio-locadora/scripts/locadora_03_DQL.sql
@@ -8,6 +8,10 @@ SELECT * FROM MODELO;
 SELECT * FROM VEICULO;
 SELECT * FROM ALUGUEL;
 
+delete from veiculo where idVeiculo = 6
+
+SELECT * FROM ALUGUEL A LEFT JOIN CLIENTE ON A.idcliente = CLIENTE.idCliente LEFT JOIN VEICULO ON A.idVeiculo = VEICULO.idVeiculo LEFT JOIN MODELO M ON VEICULO.idModelo = M.idModelo LEFT JOIN EMPRESA ON VEICULO.idEmpresa = EMPRESA.idEmpresa LEFT JOIN MARCA ON M.idMarca = MARCA.idMarca
+
 SELECT nomeEmpresa, nomeModelo, placa FROM VEICULO LEFT JOIN EMPRESA ON EMPRESA.idEmpresa = VEICULO.idEmpresa LEFT JOIN MODELO ON MODELO.idModelo = VEICULO.idEmpresa WHERE idVeiculo = 2;
 
 SELECT A.idVeiculo, dataRetirada, dataDevolucao, CLIENTE.nomeCliente, M.idModelo, M.nomeModelo 
